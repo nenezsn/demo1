5147d427cf995c904fc47884f27feb3c5baa3628
wangbing <wangbing@goyoo.com>
2017-06-01 14:54:16 +0800

    最终版

--- js/select.js
+++ js/select.js
@@ -2,5 +2,5 @@ var selected =document.getElementById("sel");
 	var img = document.getElementById("tulip");
 	selected.onchange=function(){
 		var value= this.value;
-		img.src="img/"+value+".jpg"
+		img.src="img/"+value
 	}
